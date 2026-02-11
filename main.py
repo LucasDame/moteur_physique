b91eb0364d14be7cc6acacef0d70e7723f4d5c9c
LucasDame <dame.lucas.dl@gmail.com>
2026-02-11 18:26:14 +0100

    sauvegarde

--- main.py
+++ main.py
@@ -36,19 +36,28 @@ class App:
 
         self.setup_scene()
 
+        self.time_start = time.time()
         self.update()
 
     def setup_scene(self):
         scenes.poly_cage_scene(self.world, self.width, self.height, PPM)
-        scenes.balls_scene(self.world, 100, self.width, self.height, PPM)
+        scenes.balls_scene(self.world, 50, self.width, self.height, PPM)
         scenes.polygon_scene(self.world, 3, self.width, self.height, PPM)
 
     def update(self):
+        buffer = self.time_start
+        self.time_start = time.time()
+        dt = self.time_start - buffer
+        real_fps = 1 / dt if dt > 0 else FPS
+
         self.world.step(DT)
         
         self.renderer.draw_world(self.world)
+        self.canvas.create_text(10, 10, text="fps: {:.2f}".format(real_fps), anchor="nw", fill="grey", font=("Arial", 16))
 
-        self.root.after(int(1000/FPS), self.update)
+        time_end = time.time()
+        elapsed = time_end - self.time_start
+        self.root.after(int(1000/FPS - elapsed * 1000), self.update)
 
 if __name__ == "__main__":
     root = tk.Tk()
